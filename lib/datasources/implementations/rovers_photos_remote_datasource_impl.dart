096bb62c46da49de23c5f7e0dd6ecef030c41284
Bruno Siqueira <brunovsiq@gmail.com>
2023-09-07 16:08:15 -0300

    creating getRoversPhotos method

--- lib/datasources/implementations/rovers_photos_remote_datasource_impl.dart
+++ lib/datasources/implementations/rovers_photos_remote_datasource_impl.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:nasa_app/datasources/interfaces/rovers_photos_remote_datasource.dart';
 import 'package:nasa_app/models/photo_model.dart';
 import 'package:nasa_app/network/interfaces/api.dart';
@@ -12,5 +13,21 @@ class RoversPhotosRemoteDatasourceImpl implements RoversPhotosRemoteDatasource {
   });
 
   @override
-  Future<List<PhotoModel>> getRoversPhotos() {}
+  Future<List<PhotoModel>> getRoversPhotos() async {
+    final String url = '$baseUrl/mars-photos/api/v1/rovers/curiosity/photos';
+    try {
+      Response response = await api.httpGet(url: url, queryParams: {
+        'sol': 1000,
+        'api_key':
+            'pAr57XMlhMYotz6Xs1H73qarbSWru8C1jP1kNEBh', //FIXME: but it on .env
+      });
+      return (response.data as List)
+          .map((e) => PhotoModel.fromJson(e))
+          .toList();
+    } on DioException catch (e, stackTrace) {
+      throw GetRoversPhotosException(dioException: e, stackTrace: stackTrace);
+    } catch (e, stackTrace) {
+      throw UnmappedException(exception: e, stackTrace: stackTrace);
+    }
+  }
 }
