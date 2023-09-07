77aadbf60a67e36593641b9214bb72f249a93e34
Bruno Siqueira <brunovsiq@gmail.com>
2023-09-07 19:46:44 -0300

    RoverModel test

---
+++ test/src/models/rover_model_test.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nasa_app/src/enums/rover_status_enum.dart';
+import 'package:nasa_app/src/models/rover_model.dart';
+
+import '../../fixtures/fixture_reader.dart'; // Make sure to import the RoverModel class.
+
+void main() {
+  group('RoverModel.fromJson', () {
+    test('should return a valid RoverModel', () {
+      // Arrange
+      final Map<String, dynamic> jsonMap =
+          jsonDecode(fixture('rover_data.json'));
+
+      final tRover = RoverModel(
+        id: 5,
+        name: "Curiosity",
+        landingDate: "2012-08-06",
+        launchDate: "2011-11-26",
+        status: RoverStatusEnum.active,
+      );
+
+      // Act
+      final result = RoverModel.fromJson(jsonMap);
+
+      // Assert
+      expect(result, equals(tRover));
+    });
+  });
+}
