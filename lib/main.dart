5ed1fdc31e8603d871eead9670f2b01580f2ddaf
Bruno Siqueira <brunovsiq@gmail.com>
2023-09-06 22:57:57 -0300

    remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:nasa_app/pages/login_page.dart';
-import 'package:nasa_app/routes.dart';
 import 'firebase_options.dart';
 
 void main() {
--- lib/services/interfaces/firebase_authentication_service.dart
+++ lib/services/interfaces/firebase_authentication_service.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nasa_app/errors/failures/base_failure.dart';
 
 abstract interface class FirebaseAuthenticationService {
